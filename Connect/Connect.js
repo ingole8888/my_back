c88a1645330f632f399d009d3524d92929b832c5
ingole8888 <ingole8888@gmail.com>
2022-12-12 21:04:02 +0530

    my server

--- Connect/Connect.js
+++ Connect/Connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const connect = () => {
-    
-    return mongoose.connect("mongodb+srv://sunil:sunil@cluster0.g6orjd8.mongodb.net/?retryWrites=true&w=majority")
+    //return mongoose.connect("mongodb+srv://Mock-server:Mock-server@cluster0.an24sqd.mongodb.net/shubham?retryWrites=true&w=majority")
+    return mongoose.connect("mongodb+srv://sunil:sunil@cluster0.g6orjd8.mongodb.net/sunil?retryWrites=true&w=majority")
 
     
 }
